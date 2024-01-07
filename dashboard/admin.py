c63a70aa369794e5fd5dce097e3c902a5652e948
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-01-07 11:46:20 +0600

    Delete dashboard directory
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

--- dashboard/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from dashboard.models import *
-
-admin.site.register(SearchHistory)
-admin.site.register(Project)
-admin.site.register(OpenAiAPIKey)
-admin.site.register(NetlasAPIKey)
